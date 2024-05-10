6ab1124ad8089943928772d715e594eb3f1ccdd4
53rus <53rus@mail.ru>
2024-05-10 23:39:13 +0300

    Initial commit

---
+++ src/test/java/proskyIntegerList/IntegerListApplicationTests.java
@@ -0,0 +1,13 @@
+package proskyIntegerList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegerListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
